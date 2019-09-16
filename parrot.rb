62604eb3efc2ebcb184a2dcc3691538d978da201
Morteza Rohaninejad <mortezaroha@gmail.com>
2019-09-16 17:54:26 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (*args)
+  case args.size
+  when 0
+    puts "Squawk!"
+    return "Squawk!"
+  when 1
+    puts args.join("")
+    return args.join("")
+  end
+end
